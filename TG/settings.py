4ecee24a2deacdcd31f4f3e1766034fbd2a86dbd
Bruno Muciño <mucinoab@gmail.com>
2020-04-03 13:07:48 -0600

    Arrelga http-https

--- TG/settings.py
+++ TG/settings.py
@@ -13,6 +13,9 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 import os
 import django_heroku
 
+SECURE_SSL_REDIRECT = True
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
