b89cbad3149adfa89a1c5a79dfd598436cd5bd1a
yollotltamayo <yollotltamayo@comunidad.unam.mx>
2020-04-03 10:43:28 -0600

    agregue las matrices pero todavia no estan al cien

--- graph/urls.py
+++ graph/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import url
 
 from . import views
-from .views import graph,returnjson
+from .views import graph,returnjson,sendjson
 
 urlpatterns = [
     url(r'^$', graph),
     url(r'^json/returnjson$', views.returnjson, name='returnjson'),
+    url(r'^json/sendjson$', views.sendjson, name='sendjson'),
 ]
--- graph/views.py
+++ graph/views.py
@@ -1,12 +1,13 @@
-from django.db import connections
-from django.db.models import Count
 from django.http import JsonResponse
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
+from sympy import *
 import networkx as nx
 import json 
+import numpy as np
 
+init_printing(use_unicode=True)
 def graph(request):
     return render(request,'graph.html')
 class matrix:
@@ -14,13 +15,25 @@ class matrix:
         G = nx.DiGraph()
         G.add_nodes_from(nodes)
         G.add_edges_from(edges)        
-        incidence_matrix = nx.incidence_matrix(G,nodelist= nodes, oriented=True) 
         adjacency_matrix = nx.adjacency_matrix(G,nodelist=nodes,weight = '1')
-        print(incidence_matrix.toarray())
-        print(A.toarray())
+        adjacency_matrix = adjacency_matrix.toarray()
+        M = Matrix(adjacency_matrix)
+        return(latex(M))
+
+    def incidencia(self,nodes,edges):
+        G = nx.DiGraph()
+        G.add_nodes_from(nodes)
+        G.add_edges_from(edges)      
+        incidence_matrix = nx.incidence_matrix(G,nodelist= nodes, oriented=True) 
+        incidence_matrix = incidence_matrix.toarray()
+        M = Matrix(incidence_matrix)
+        return(latex(M))
+        #print(incidence_matrix.toarray())
+
 
 @csrf_exempt
 def returnjson(request):
+    context = {'a':0}
     if request.is_ajax():
         data = json.loads(request.body)
         nodos  = []
@@ -29,10 +42,13 @@ def returnjson(request):
             nodos.append(data[a]['nodo'])
             for b in range(0,len(data[a]['lista'])):
                 edges.append([data[a]['nodo'],   data[a]['lista'][b]])
-        print(edges)
-        print(nodos)
-        ady = matrix()
-        ady.adyacencia(nodos,edges)
+        m = matrix()
+        context['adya'] = m.adyacencia(nodos,edges)
+        context['inci'] = m.incidencia(nodos,edges)
+        request.session['context'] = context
         return HttpResponse("ok")
 
-
+def sendjson(request):
+    context = request.session["context"]
+    print(context)
+    return JsonResponse(context,safe=False) 
