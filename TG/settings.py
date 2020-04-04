4bc43ba9da9d5ee35908be08b58def1a415c5899
Bruno Muciño <mucinoab@gmail.com>
2020-04-03 19:30:29 -0600

    Cambio de variable hhtps

--- TG/settings.py
+++ TG/settings.py
@@ -14,8 +14,8 @@ import os
 
 import django_heroku
 
-SECURE_SSL_REDIRECT = False
-# SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_SSL_REDIRECT = True
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
