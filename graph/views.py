0d35718ea8a49b4bf4408dd671277d87ea5cee8f
yollotltamayo <yollotltamayo@comunidad.unam.mx>
2020-05-23 11:17:01 -0500

    quite algunos bugs

--- graph/views.py
+++ graph/views.py
@@ -1,42 +1,75 @@
 import json
-
 import networkx as nx
 from django.http import HttpResponse
 from django.http import JsonResponse
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 from sympy import *
-
+import numpy as np
+import pandas
 
 def graph(request):
     return render(request, 'graph.html')
 
 
 class matrix:
-    def adyacencia(self, nodes, edges):
+    def adyacencia(self, nodes, edges,edgesc):
         import re
         G = nx.DiGraph()
         G.add_nodes_from(nodes)
         G.add_edges_from(edges)
-        adjacency_matrix = nx.adjacency_matrix(G, nodelist=nodes, weight='1')
+        adjacency_matrix =  nx.adjacency_matrix(G, nodelist=nodes, weight='1')
         adjacency_matrix = adjacency_matrix.toarray()
+        #print(adjacency_matrix)
         M = latex(Matrix(adjacency_matrix))
-        M = re.sub(r'[1]', r'\\textbf{1}', M)
+        nodes = np.array(nodes)
+        edgesc = np.array(edgesc)
+        adjacency_matrix = np.array(adjacency_matrix)
 
+        b = np.zeros((len(nodes) + 1,len(nodes) + 1),object)
+        #df = pandas.DataFrame(adjacency_matrix, columns=nodes, index=nodes)        
+        
+
+        b[1:,1:]=adjacency_matrix
+        b[0,1:]=nodes
+        b[1:,0]=nodes
+        b[0,0]='100000000001'
+
+        M = latex(Matrix(b))
+        M = M.replace("100000000001"," ")
+        #M = re.sub(r'[1]', r'\\textbf{1}', M)
         return M
 
-    def incidencia(self, nodes, edges):
+    def incidencia(self, nodes, edges,edgesc):
         import re
         G = nx.DiGraph()
         G.add_nodes_from(nodes)
         G.add_edges_from(edges)
         incidence_matrix = nx.incidence_matrix(G, nodelist=nodes, oriented=True)
         incidence_matrix = incidence_matrix.toarray()
+        print(incidence_matrix)
+        
+        nodes = np.array(nodes)
+        edgesc = np.array(edgesc)
+        incidence_matrix  = np.array(incidence_matrix)        
+
+        b = np.zeros((len(nodes) + 1,len(edgesc) + 1),object)
+
+        b[1:,1:]=incidence_matrix
+        b[0,1:]=edgesc
+        b[1:,0]=nodes
+        b[0,0]='100000000001'
+        #printer = np.vectorize(lambda edgesc:'{0:5}'.format(edgesc,))
+        #nodes = nodes.astype(str)
+        print (latex(Matrix(b)))
+        #df = pandas.DataFrame(incidence_matrix, columns=edgesc, index=nodes).to_numpy()
+        #print(df) # 
+        n = latex(Matrix(b))
+        n = n.replace("100000000001"," ")
         M = latex(Matrix(incidence_matrix))
         M = re.sub(r'1\.0', r'\\textbf{1}', M)  # s
         M = re.sub(r'0\.0', r'0', M)  # ceros
-
-        return M
+        return n
 
 
 @csrf_exempt
@@ -46,14 +79,16 @@ def returnjson(request):
         data = json.loads(request.body)
         nodos = []
         edges = []
+        edgesc = [  ]
         for a in range(0, len(data)):
-            nodos.append(data[a]['nodo'])
+            nodos.append(int(data[a]['nodo']))
             for b in range(0, len(data[a]['lista'])):
-                edges.append([data[a]['nodo'], data[a]['lista'][b]])
-        # print(nodos, edges)
+                edges.append( [int(data[a]['nodo']), int(data[a]['lista'][b])])
+                edgesc.append(str(data[a]['nodo']) + ','+str(data[a]['lista'][b]))
+        #print(nodos, edges)
         m = matrix()
-        context['adya'] = m.adyacencia(nodos, edges)
-        context['inci'] = m.incidencia(nodos, edges)
+        context['adya'] = m.adyacencia(nodos, edges,edgesc)
+        context['inci'] = m.incidencia(nodos, edges,edgesc)
         request.session['context'] = context
         return HttpResponse("ok")
 
